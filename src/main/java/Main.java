a990a918529f75c072affb7c2ed1b891f894b0c8
Roman Kaminskiy - notebook <derrki@gmail.com>
2019-02-14 23:36:35 +0200

    Додано інтерфейси для уніфікації виводу Hello!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        if(args.length>0){
+            for (int i = 0; i<args.length; i++){
+            System.out.print(args[i] + " ");
+            }
+        }
+        else {
+            System.out.println("Hello world");
+        }
     }
 }
